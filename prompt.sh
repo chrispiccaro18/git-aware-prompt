163a54378f7a7561cf3f74cb4dc7e361b7310492
Mike Weilgart <mike.weilgart@verticalsysadmin.com>
2016-03-18 22:01:16 -0700

    Quote command expansion

--- prompt.sh
+++ prompt.sh
@@ -1,7 +1,7 @@
 find_git_branch() {
   # Based on: http://stackoverflow.com/a/13003854/170413
   local branch
-  if branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null); then
+  if branch="$(git rev-parse --abbrev-ref HEAD 2> /dev/null)"; then
     if [[ "$branch" == "HEAD" ]]; then
       branch='detached*'
     fi
@@ -12,7 +12,7 @@ find_git_branch() {
 }
 
 find_git_dirty() {
-  local status=$(git status --porcelain 2> /dev/null)
+  local status="$(git status --porcelain 2> /dev/null)"
   if [[ "$status" != "" ]]; then
     git_dirty='*'
   else
